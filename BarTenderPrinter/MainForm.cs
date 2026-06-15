5374109520222607221905e5b328bfe14c7cc311
tall-1997 <tall-1997@users.noreply.github.com>
2026-06-15 16:23:46 +0000

    feat: 迁移到.NET 8自包含单文件发布，无需安装运行时

--- BarTenderPrinter/MainForm.cs
+++ BarTenderPrinter/MainForm.cs
@@ -15,7 +15,7 @@ namespace BarTenderPrinter
         private readonly BarTenderService _btService = new BarTenderService();
         private readonly HistoryManager _history = new HistoryManager();
         private readonly string _configFile;
-        private readonly string _version = "v4.2.0";
+        private readonly string _version = "v4.5.0";
 
         private List<DataSourceItem> _dataSources = new List<DataSourceItem>();
         private TextBox[] _inputTextBoxes = new TextBox[0];
